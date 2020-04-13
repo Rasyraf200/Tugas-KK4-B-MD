3aa668f1dd906b2a1b25a54611a3ed4f5d8b2243
Rakha Asyraf <rasyraf7@gmail.com>
2020-04-13 16:17:17 +0700

    reinitialized files

---
+++ app/src/main/java/com/example/androiddasar29navigationdrawer/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.androiddasar29navigationdrawer;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    ImageView ivlogo;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ivlogo = view.findViewById(R.id.iv_logo);
+        ivlogo.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(getActivity(), "Tes Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+}
